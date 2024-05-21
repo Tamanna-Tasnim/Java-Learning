0d9a427cc8c6ee3e71f13704275a5cf22a21039e
Tamanna-Tasnim <tamannatasnim20t@gmail.com>
2024-05-21 13:00:52 +0600

    new

--- ifcondition.java
+++ ifcondition.java
@@ -154,8 +154,6 @@ public class ifcondition
             case 5: System.out.println("Wednesday");
             break;
             case 6: System.out.println("Thursday");
-
-            
             break;
             case 7: System.out.println("Friday");
             break;
