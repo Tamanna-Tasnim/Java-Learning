d9b0406a1f8f7acd7296d6016eadd63c21ac72e1
Tamanna-Tasnim <tamannatasnim20t@gmail.com>
2024-05-22 16:33:37 +0600

    Looping

---
+++ looppractise.java
@@ -0,0 +1,84 @@
+public class looppractise 
+{
+    public static void main(String[] args) 
+    {
+//print 1-10 using while loop
+        /*int i=1;
+        while(i<=10)
+        {
+           System.out.println(i);
+
+           i++;
+        } */ 
+        
+        
+//print even number between 1-10
+        /*int i=2;
+        while(i<=10)
+        {
+           System.out.println(i);
+           i+=2;
+        } */
+        
+        /*int i=1; 
+        while(i<=10)
+        {
+           if(i%2==0)
+           {
+            System.out.println(i);
+           }
+        i++;
+        } */
+
+        /*int i=1; 
+        while(i<=10)
+        {
+           if(i%2==0)
+           {
+            System.out.println(i +"Even");
+           }
+           else
+           {
+            System.out.println(i+"Odd");
+           }
+        i++;
+        } */
+
+        /*int i=10; //print 10...1 in decreasing order
+        while(i>0)
+           {
+            System.out.println(i);
+            i--;
+           } */
+         
+
+//Do while loop       
+        /*int i=1;
+        do
+        {
+         System.out.println(i);
+         i++;
+        }
+        while(i<=10);*/
+
+        int i=10;
+        do
+        {
+         System.out.println(i);
+         i--;
+        }
+        while(i>=1);
+
+        
+
+
+
+
+
+
+
+
+
+    }
+    
+}
