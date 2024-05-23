ba752ca7546d47f708ec666342e093a855ffb8a4
Tamanna-Tasnim <tamannatasnim20t@gmail.com>
2024-05-23 12:34:21 +0600

    new

--- looppractise.java
+++ looppractise.java
@@ -91,7 +91,24 @@ public class looppractise
          System.out.println();      
       }
      
-         
+     
+//palindrome number
+
+      int r,sum=0,temp;    
+      int n=454; 
+
+      temp=n;    
+      while(n>0)
+      {
+         r=n%10;   
+         sum=(sum*10)+r;    
+         n=n/10;    
+      }    
+      if(temp==sum)
+      System.out.println("palindrome number ");    
+      else    
+      System.out.println("not palindrome"); 
+      
 
    }
     
